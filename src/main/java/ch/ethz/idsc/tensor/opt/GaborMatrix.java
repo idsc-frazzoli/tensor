9ea1ca7197f7ed500f5c4ebf1011eb1bb3a93d0d
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-07-02 18:14:48 +0200

    minor edits

--- src/main/java/ch/ethz/idsc/tensor/opt/GaborMatrix.java
+++ src/main/java/ch/ethz/idsc/tensor/opt/GaborMatrix.java
@@ -1,9 +1,37 @@
 // code by jph
 package ch.ethz.idsc.tensor.opt;
 
+import ch.ethz.idsc.tensor.RationalScalar;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
+import ch.ethz.idsc.tensor.red.Norm;
+import ch.ethz.idsc.tensor.sca.AbsSquared;
+import ch.ethz.idsc.tensor.sca.Cos;
+import ch.ethz.idsc.tensor.sca.Exp;
+
 /** inspired by
  * <a href="https://reference.wolfram.com/language/ref/GaborMatrix.html">GaborMatrix</a> */
-enum GaborMatrix {
+public enum GaborMatrix {
   ;
-  // TODO implement
+  /** only approximately consistent with Mathematica
+   * 
+   * @param r
+   * @param k
+   * @param phi
+   * @return */
+  // TODO not tested
+  public static Tensor of(int r, Tensor k, Scalar phi) {
+    final Scalar sigma = RationalScalar.of(r, 2);
+    final Scalar factor = AbsSquared.of(sigma).multiply(RealScalar.of(2)).invert().negate();
+    final int m = 2 * r + 1;
+    final Tensor offset = Tensors.vector(-r, -r);
+    Tensor matrix = Array.of(list -> Norm._2SQUARED.of(Tensors.vector(list).add(offset)), m, m) //
+        .multiply(factor).map(Exp.FUNCTION);
+    Tensor weight = Array.of(list -> k.dot(Tensors.vector(list).add(offset)).subtract(phi), m, m) //
+        .map(Cos.FUNCTION);
+    return weight.pmul(matrix);
+  }
 }
--- src/test/java/ch/ethz/idsc/tensor/opt/GaborMatrixTest.java
+++ src/test/java/ch/ethz/idsc/tensor/opt/GaborMatrixTest.java
@@ -3,19 +3,16 @@ package ch.ethz.idsc.tensor.opt;
 
 import java.util.Arrays;
 
+import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Dimensions;
 import junit.framework.TestCase;
 
-/** [
- * [ 0.0113 0.0838 0.0113 ]
- * [ 0.0838 0.6193 0.0838 ]
- * [ 0.0113 0.0838 0.0113 ]
- * ] */
-public class GaussianMatrixTest extends TestCase {
+public class GaborMatrixTest extends TestCase {
   public void testSimple() {
-    Tensor mat = GaussianMatrix.of(1);
-    assertEquals(Dimensions.of(mat), Arrays.asList(3, 3));
+    Tensor mat = GaborMatrix.of(2, Tensors.vector(.2, .1), RealScalar.of(.2));
     // System.out.println(Pretty.of(mat.map(Round._4)));
+    assertEquals(Dimensions.of(mat), Arrays.asList(5, 5));
   }
 }
